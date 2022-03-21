51225bf0cd7a27324788ed4dc731b8042c7364b8
kamdaou <you@example.com>
2022-03-21 21:10:23 +0200

    added files

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,15 @@
-/*
-*swap_int(int, int)
-*void
-*/
+#include "main.h"
+/**
+ *swap_int - swap value of two ints.
+ *@a: pointer to one int.
+ *@b: pointer to second int.
+ *
+ *Return: void.
+ */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,19 @@
-/*
-*_strlen(char *s)
-*return int
-*/
+#include "main.h"
+/**
+ *_strlen - finds and outputs length of string.
+ *@s: pointer to string.
+ *
+ *Return: length of a string.
+ */
 int _strlen(char *s)
 {
-	return strlen(s);
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
