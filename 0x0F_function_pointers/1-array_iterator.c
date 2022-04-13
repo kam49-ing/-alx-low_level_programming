740a461289140afc6f8409d297c9bfa0768daefe
root <you@example.com>
2022-04-13 19:09:02 +0100

    created 1-array_iterator

---
+++ 0x0F_function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+/**
+ * array_iterator - execute a given function as a parameter on each element of an array
+ * @array: array of int
+ * @size: size of array
+ * @(*action)(int): the given function
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	i = 0;
+	for (i = 0; i < (int) size; i++) {
+		action(array[i]);
+	}
+}
