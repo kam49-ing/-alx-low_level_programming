edf000c30dd630b55618feb37ccec5f6abd8a19f
root <you@example.com>
2022-04-13 22:05:41 +0100

    refractor

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,27 +2,22 @@
 
 /**
  *
- * @param array: array of int
- * @param size: size of the array
- * @param cmp: a pointer to the function to be used to compare values
+ * @array: array of int
+ * @size: size of the array
+ * @cmp: a pointer to the function to be used to compare values
  *
- * Return: int.
+ * Return: index of the element or -1 if size <= 0 or if the element is not gotten.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int comparison;
+
 	if (size <= 0)
-	{
 		return (-1);
-	}
+
 	for (i = 0; i < size; i++)
-	{
-		comparison = cmp(array[i]);
-		if (comparison != 0)
-		{
+		if (cmp(array[i]) != 0)
 			return (i);
-		}
-	}
+
 	return (-1);
 }
