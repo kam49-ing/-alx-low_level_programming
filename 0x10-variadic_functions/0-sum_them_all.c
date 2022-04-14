da0e9cfef4cc3eb76d844f029c468cc682ced99a
root <you@example.com>
2022-04-14 12:19:07 +0100

    created sum them all function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+/**
+ *
+ * sum_them_all - sum an unknown number of integers
+ * @n: first parameter
+ * ...: variadics parameters
+ *
+ * Return: 0 if n <= 0, the sum of parameters otherwise
+ */
+fun sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list ap;
+	int i;
+	
+	va_start(ap, count);
+
+	for (i = 0; i < count; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
+
