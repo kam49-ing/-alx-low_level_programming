c6f5fb9eb7c131d69fa3153ff9c833b0225d9e2d
root <you@example.com>
2022-04-14 20:33:05 +0100

    refractor

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	va_list ap;
-	int i;
+	unsigned int i;
 	
-	va_start(ap, count);
+	va_start(ap, n);
 
-	for (i = 0; i < count; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
 	return (sum);
 }
-
