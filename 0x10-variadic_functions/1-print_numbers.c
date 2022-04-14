21b8906aa8d70f8f128ab288d4ea5bb7aa280930
root <you@example.com>
2022-04-14 22:12:48 +0100

    refractor

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
+
 		if (separator != NULL && i + 1 < n)
 			printf("%s", separator);
 	}
 
 	va_end(ap);
+
 	printf("\n");
 }
