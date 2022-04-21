e87c6397518004a4de0b7617c05867c06dedce9d
root <you@example.com>
2022-04-21 10:12:01 +0100

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - return number of elements in a given linked list
+ *
+ * @h: head of the list
+ *
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int n = 0;
+
+	if (!h)
+		return (-1);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
