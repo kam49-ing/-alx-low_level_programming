eaca94fc846d11026705280bb45baba4801fbd10
root <you@example.com>
2022-04-21 19:45:34 +0100

    refractored

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,13 +11,11 @@ void free_list(list_t *head)
 {
 	list_t *tmp;
 
-	if (head == NULL)
-		return;
-
 	while (head != NULL)
 	{
 		tmp = head;
 		head = head->next;
 		free(tmp);
 	}
+	free(head);
 }
