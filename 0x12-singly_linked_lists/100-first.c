2ac799999157bc0d5f920189a951bd697c277f05
root <you@example.com>
2022-04-21 19:58:53 +0100

    created startup function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * startup_fun - Prints a string before main is executed
+ *
+ * Return: nothing
+ */
+void startup_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n");
+}
