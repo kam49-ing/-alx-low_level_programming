ab04f09043323d618181d107eec3ba1616d5a541
root <you@example.com>
2022-04-29 00:49:16 +0200

    refractored code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,22 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_binary - Prints the binary representation of a number
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
  *
- * @n: the number
- *
- * Return: Nothing.
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	printf("%ld", n & 1);
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
