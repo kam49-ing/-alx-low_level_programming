342caaf1bb0d5a5e86d76f2ff7fd997376813fb7
root <you@example.com>
2022-05-04 16:06:10 +0100

    added betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output.
+ *
  * @filename: the file name
  * @letters: the number of letters it should read and print
+ *
  * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
 		n++;
-	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (n == 0)
