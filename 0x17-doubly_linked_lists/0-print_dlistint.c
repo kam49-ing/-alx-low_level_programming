d3f31b0a664e72fb5d13d6620125d75bb3837c6e
root <you@example.com>
2022-06-09 11:37:15 +0200

    refractor the code to make the name of the function in the comment the same that the one in the header and the function body.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_dlistint_t - prints all the elements of a dlistint_t list
+ * print_dlistint - prints all the elements of a dlistint_t list
  * @h: head of the list
  *
  * Return: the number of nodes
