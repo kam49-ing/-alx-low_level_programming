5cd12cb41d314bca88ea6c49b75dc4f154f307aa
root <you@example.com>
2022-06-09 23:10:05 +0200

    add prototype of functions in the header

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,4 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-#endif /* C_PROGRAMMING_LISTS_H*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+unsigned int dlistint_len2(dlistint_t *h);
+#endif /* C_PROGRAMMING_LISTS_H */
