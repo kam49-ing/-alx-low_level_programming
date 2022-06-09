71b4ea2b9df295866b345bc689cbdb37b5521435
root <you@example.com>
2022-06-09 11:47:15 +0200

    add function that add an element at the beginning of a linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define C_PROGRAMMING_LISTS_H
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,4 +22,5 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 #endif /* C_PROGRAMMING_LISTS_H*/
